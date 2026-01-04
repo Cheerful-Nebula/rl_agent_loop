97038777ff23845424e79b0ff5a2b07f7d05c2e3
Cheerful-Nebula <115181451+Cheerful-Nebula@users.noreply.github.com>
2026-01-04 12:41:06 -0500

    part of the prompt building refactor

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -2,7 +2,6 @@
 from .builders import build_diagnosis_prompt
 from .builders import build_coding_prompt
 from .builders import build_fix_prompt
-from .builders import build_multi_diagnosis_prompt
 
 
 # This allows: from prompts import load_template (if needed)
